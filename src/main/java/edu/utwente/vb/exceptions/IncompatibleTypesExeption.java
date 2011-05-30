405166589635713852f196b20f4a3d9f841dbeb3
Niek Tax <n.tax@student.utwente.nl>
2011-05-30 20:35:24 +0200

    Eigen exceptie gemaakt voor conflicterende dataypen

---
+++ src/main/java/edu/utwente/vb/exceptions/IncompatibleTypesExeption.java
@@ -0,0 +1,15 @@
+package edu.utwente.vb.exceptions;
+
+public class IncompatibleTypesExeption extends Exception {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1185659439434144841L;
+
+	public IncompatibleTypesExeption(String message) {
+		super(message);
+	}
+
+}
