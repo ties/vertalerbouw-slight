6827489e3cdabd24edef66bd37a63f18df4acb84
Niek Tax <n.tax@student.utwente.nl>
2011-06-13 20:33:10 +0200

    nu tevreden met test-coverage

--- src/test/resources/const.ex
+++
@@ -1,5 +0,0 @@
-# Definition of constants
-
-const int a = 3
-const char b = 'b'
-const string c = "String"
--- src/test/resources/while_braces.ex
+++ src/test/resources/while_braces.ex
@@ -5,5 +5,4 @@
 #
 int i = 10
 while(i > 0):
-	print("Is " + i + " even?" + i % 2)
 	i = i - 1
